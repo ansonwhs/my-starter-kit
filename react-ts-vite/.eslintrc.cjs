775454cdd81e3d1ca632c908170904c300564c4f
Bynd-Git <111232480+Bynd-Git@users.noreply.github.com>
2024-04-06 00:31:22 +0800

    feat: complete tsconfg, viteconfig and some eslint

--- react-ts-vite/.eslintrc.cjs
+++ react-ts-vite/.eslintrc.cjs
@@ -6,13 +6,77 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/**/*.test.ts', 'src/**/*.test.tsx'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'array-callback-return': [
+      'error',
+      {
+        checkForEach: true,
+        allowVoid: true,
+      }
+    ],
+    // Possible Problems
+    'no-await-in-loop': 'error',
+    'no-constant-binary-expression': 'error',
+    'no-duplicate-imports': 'error',
+    'no-promise-executor-return': 'warn',
+    'no-self-compare': 'error',
+    'no-template-curly-in-string': 'warn',
+    'no-unmodified-loop-condition': 'error',
+    'no-unreachable-loop': 'error',
+    'no-use-before-define': 'error',
+    'require-atomic-updates': 'error',
+    // Suggestions
+    'arrow-body-style': [
+      'error',
+      'as-needed'
+    ],
+    'block-scoped-var': 'error',
+    'camelcase': [
+      'error',
+      {
+        properties: 'always',
+        ignoreImports: true,
+        ignoreGlobals: true,
+      },
+    ],
+    'complexity': [
+      'error',
+      15
+    ],
+    'consistent-return': [
+      'error',
+      {
+        treatUndefinedAsUnspecified:
+          true
+      },
+    ],
+    'curly': [
+      'error',
+      'multi-line',
+    ],
+    'default-case': 'error',
+    'default-case-last': 'error',
+    'default-param-last': 'error',
+    'dot-notation': 'error',
+    'eqeqeq': [
+      'error',
+      'always',
+    ],
+    'func-name-matching': 'warn',
+    'func-names': 'warn',
+    'func-style': [
+      'warn',
+      'declaration',
+      {
+        allowArrowFunctions: true,
+      },
     ],
   },
 }
--- react-ts-vite/src/App.tsx
+++ react-ts-vite/src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
 
-  return (
-    <h1 className="underline">
-      {import.meta.env.VITE_APPLICAITON_NAME}
-    </h1>
-  )
+    return (
+        <h1 className="underline">
+            {import.meta.env.VITE_APPLICAITON_NAME}
+        </h1>
+    )
 }
 
 export default App
