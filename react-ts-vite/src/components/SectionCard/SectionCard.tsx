aea64605e3458265123a11408b6f9ea250ec6d3e
Bynd-Git <111232480+Bynd-Git@users.noreply.github.com>
2024-04-08 00:16:59 +0800

    push: prettier

--- react-ts-vite/src/components/SectionCard/SectionCard.tsx
+++ react-ts-vite/src/components/SectionCard/SectionCard.tsx
@@ -2,14 +2,14 @@ import { Card, CardTitle, CardBody } from '@ui/Card';
 import type { PropsWithChildren } from 'react';
 
 type SectionCardProps = PropsWithChildren<{
-    title: string;
-}>
+	title: string;
+}>;
 
 const SectionCard = (props: SectionCardProps): JSX.Element => (
-    <Card >
-        <CardTitle>{props.title}</CardTitle>
-        <CardBody>{props.children}</CardBody>
-    </ Card>
+	<Card>
+		<CardTitle>{props.title}</CardTitle>
+		<CardBody>{props.children}</CardBody>
+	</Card>
 );
 
 export default SectionCard;
