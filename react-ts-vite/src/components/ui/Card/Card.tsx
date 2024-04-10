a8b5df23e99667b1d5341bc0cc4e250e1633de6a
Bynd-Git <111232480+Bynd-Git@users.noreply.github.com>
2024-04-10 21:19:24 +0800

    feat: complete design

--- react-ts-vite/src/components/ui/Card/Card.tsx
+++ react-ts-vite/src/components/ui/Card/Card.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 const Card = (props: { children: ReactNode }): JSX.Element => (
-	<div className='h-80 bg-yellow-50 my-3 mx-10 border-solid border-blue-200 border-2 rounded-lg hover:border-4 hover:border-blue-400'>
+	<div className='h-80 w-5/6 bg-yellow-50 my-3 mx-10 border-solid border-blue-200 border-2 rounded-lg hover:border-4 hover:border-blue-400'>
 		{props.children}
 	</div>
 );
--- react-ts-vite/src/screens/Home/DescriptionSection.tsx
+++ react-ts-vite/src/screens/Home/DescriptionSection.tsx
@@ -4,6 +4,10 @@ const Important = (props: { children: string }): JSX.Element => (
 	<span className='font-bold text-rose-400'>{props.children}</span>
 );
 
+const SubItemTitle = (props: { children: string }): JSX.Element => (
+	<p className='underline py-2 font-semibold'>{props.children}</p>
+);
+
 const TsConfigSection = (): JSX.Element => (
 	<SectionCard title='Typescript Configuration'>
 		<div>
@@ -76,20 +80,81 @@ const EslintConfigSection = (): JSX.Element => (
 				This is the most critical configuration in this starter kit. In
 				order to keep code quality, differenet ESLINT rules has setup.
 			</p>
-			<p className='underline py-2'>Plain JavaScript Eslint</p>
+			<SubItemTitle>Plain JavaScript Eslint</SubItemTitle>
+			<p>
+				There is no ESLINT recomendation make use in Plain Javascript.
+				All rules are self configured to match with best practice for my
+				own. There is total 97 rules are configured in{' '}
+				<b>esling.config.js</b>
+			</p>
+			<SubItemTitle>Typescript Eslint</SubItemTitle>
+			<p>
+				There is just a simple plugin{' '}
+				<Important>typescript-eslint</Important> installed for
+				Typescript. No additional rules configured and simply using
+				recommended rules.
+			</p>
+			<SubItemTitle>React Related</SubItemTitle>
+			<p>
+				In this section, it configures with <b>React</b> related ESLINT.
+				3 plugins are installed:
+			</p>
+			<ul className='list-decimal pl-5 my-2'>
+				<li>eslint-plugin-react</li>
+				<li>eslint-plugin-react-refresh</li>
+				<li>eslint-plugin-react-hooks</li>
+			</ul>
+			<p>
+				There is no much custom configuration in second and third
+				plugins. So most of the rules are about reacts. According to
+				documents, it looks more like more concerned on{' '}
+				<Important>Code Formatting and Props Standard</Important>. And
+				so that most of the rules can be fixed by command <b>--fix</b>{' '}
+				to complete.
+			</p>
+			<SubItemTitle>JSX A11y</SubItemTitle>
+			<p>
+				This plugin is obviously more concerned on JSX styling and
+				linting. However, you will see there is a little of custom rules
+				configure in <b>eslint.config.js</b>. Most of rules are in{' '}
+				<Important>Recommended Rules</Important> and which is all very
+				useful and no need to overwrite.
+			</p>
+			<SubItemTitle>Preitter</SubItemTitle>
+			<p>
+				A preitter eslint plugin is installed to prevent contradiction
+				from <b>eslint recommended</b> and <b>Preitter configuration</b>
+				.
+			</p>
 		</div>
 	</SectionCard>
 );
 
-const DescriptionSection = (): JSX.Element => (
-	<div className='grid grid-cols-1 md:grid-cols-2 justify-items-center gap-2'>
-		<TsConfigSection />
-
-		<ViteConfigSection />
-
-		<PrettierConfigSection />
-		<EslintConfigSection />
+const Footer = (): JSX.Element => (
+	<div className='m-5 text-center text-sm italic'>
+		<p>
+			Some configuration like Typescript, Eslint and Prettier can wisely
+			use in different project. Kindly note that, these configuration are
+			just my personal practice on coding and suggestions from my past
+			project experience. It will not be frequently updated but it should
+			more or less unchanged throughout time.
+		</p>
 	</div>
+);
+
+const DescriptionSection = (): JSX.Element => (
+	<>
+		<div className='grid grid-cols-1 md:grid-cols-2 justify-items-center gap-2'>
+			<TsConfigSection />
+
+			<ViteConfigSection />
+
+			<PrettierConfigSection />
+
+			<EslintConfigSection />
+		</div>
+		<Footer />
+	</>
 );
 
 export default DescriptionSection;
